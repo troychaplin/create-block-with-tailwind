ffced3efa50df736006081976358923f695d1a7c
Troy Chaplin <troy.chaplin@gmail.com>
2024-12-11 07:43:08 -0500

    add: base plugin using create block script

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ed7356cff71873d2b2b1');
---
+++ build/view.js
@@ -0,0 +1,1 @@
+console.log("Hello World! (from create-block-create-block-with-tailwind block)");
