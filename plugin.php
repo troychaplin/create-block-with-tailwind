32012c675be1f88372aeea0fbc8d65e37760a2c8
Troy Chaplin <troy.chaplin@gmail.com>
2024-12-11 07:48:34 -0500

    refactor: main php file

--- plugin.php
+++ plugin.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
- * Plugin Name:       Create Block With Tailwind
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       Tailwind Block
+ * Description:       Example block scaffolded with Create Block tool with Tailwind integration.
  * Requires at least: 6.6
  * Requires PHP:      7.2
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Troy Chaplin
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       create-block-with-tailwind
- *
- * @package CreateBlock
+ * Text Domain:       tailwind-block
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
@@ -24,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function create_block_create_block_with_tailwind_block_init() {
+function tc_tailwind_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
-add_action( 'init', 'create_block_create_block_with_tailwind_block_init' );
+add_action( 'init', 'tc_tailwind_block_init' );
